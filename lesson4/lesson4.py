288bff7e06ffb2fe4100b04921b47815af7339d0
leoleegithubgoodboy <leolee19800206@gmail.com>
2024-09-07 15:37:30 +0800

    錯誤判斷

---
+++ lesson4/lesson4.py
@@ -0,0 +1,12 @@
+#if
+
+# age = 17
+# if age < 17:
+#     print("不可以考駕照")
+# else:
+#     print("可以考駕照")
+# print("程式結束")
+
+sorce = int(input("請輸入學生分數(最高300分)"))
+is_add = input("是否符合條件(y/n)")
+print(sorce,is_add)
