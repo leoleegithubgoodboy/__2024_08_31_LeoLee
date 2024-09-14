89dac1d45240d90b61ff81dbfb759b169bbb0ec6
leoleegithubgoodboy <leolee19800206@gmail.com>
2024-09-14 14:42:13 +0800

    lesson5_3 程式修改4

--- lesson5/lesson5_3.py
+++ lesson5/lesson5_3.py
@@ -1,15 +1,15 @@
-import tool
+#import tool
+import widget
 while True:
 
     kg=0  #清除變數
     cm=0  #清除變數
-    cm,kg = tool.input_data() #呼叫funtion
+    cm,kg = widget.input_data() #呼叫funtion
     print(f'身高={cm},體重={kg}')
-    BMI = tool.calculate_bmi(kg=kg,cm=cm) #引述名稱呼叫，可以不用依照順序
-    print(tool.get_status(BMI)) #列印狀態
+    BMI = widget.calculate_bmi(kg=kg,cm=cm) #引述名稱呼叫，可以不用依照順序
+    print(widget.get_status(BMI)) #列印狀態
     print(f'BMI={BMI}')
     play_agin = input("還要繼續嗎?(y/n)")
     if play_agin == "n":
         break
 print('程式結束')
-    
