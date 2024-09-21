e397ef25e169f3fac6c1c6f7a24df61e5278bc34
leoleegithubgoodboy <leolee19800206@gmail.com>
2024-09-21 13:10:30 +0800

    led time

--- picow2/main2.py
+++ picow2/main2.py
@@ -1,7 +1,18 @@
 #from machine import Pin
 import machine
+import time
 #led = Pin("LED",Pin.OUT)
 #led = machine.Pin("LED",machine.Pin.OUT)
 led = machine.Pin("LED",mode = machine.Pin.OUT)
 #led.value(1)
-led.on()
+status = True
+
+while True:
+    led.on()
+    if status == False:
+        led.on()
+        status = True
+    else:
+        led.off()
+        status = False
+    time.sleep(1)
