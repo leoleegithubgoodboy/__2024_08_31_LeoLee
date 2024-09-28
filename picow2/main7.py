4f756c80b2ea66f7ab94750bd0d8d55a89fc07c8
leoleegithubgoodboy <leolee19800206@gmail.com>
2024-09-28 14:11:06 +0800

    ADC Timer RTC

---
+++ picow2/main7.py
@@ -0,0 +1,41 @@
+from machine import Pin, PWM, ADC , Timer
+
+
+"""
+範例一 用時間暫停來做動作(系統暫停在做事)
+import machine 
+import time
+
+adc = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+while True:    
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    print()
+    print(f"電壓:{reading}")
+    print(f"溫度:{temperature}")
+    time.sleep(3)
+"""
+
+
+#範例二 用每隔幾次來做動作 (系統持續做當時間到才做動作)
+
+
+adc = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+def do_thing(t1):
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    print()
+    print(f"電壓:{reading}")
+    print(f"溫度:{temperature}")
+
+def do_thing1(t2):
+    adc1 = ADC(Pin(26))
+    duty1 = adc1.read_u16()
+    print()
+    print(f"可變電阻{round(duty1/65535*10)}")
+
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing )
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1 )
+
---
+++ picow2/main8.py
@@ -0,0 +1,28 @@
+from machine import Pin, PWM, ADC , Timer ,RTC
+
+
+rtc = RTC()
+adc = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+def do_thing(t1):
+    reading = adc.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    print()
+    print(f"電壓:{reading}")
+    print(f"溫度:{temperature}")
+    (year, month, day, hour, minute, second, microsecond, tzinfo)=rtc.datetime()
+    datetime_str=f"{year}-{month}-{day}  {hour}:{minute}:{second}:{microsecond}_{tzinfo}"
+    print(datetime_str)
+
+def do_thing1(t2):
+    pwm1 = PWM(Pin(15),freq=1000)
+    adc1 = ADC(Pin(26))
+    duty1 = adc1.read_u16()
+    #pwm1.freq(1000)
+    pwmd = pwm1.duty_u16(duty1)
+    print()
+    #print(f"可變電阻{round(duty1/65535*10)}")
+    print(f"{duty1}")
+    
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing )
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1 )
