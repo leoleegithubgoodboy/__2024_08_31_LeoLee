568e27a9b028d790f5cce5b01ff2dd4efdba2744
leoleegithubgoodboy <leolee19800206@gmail.com>
2024-09-28 11:17:59 +0800

    Timer ADC 兩個

--- picow2/main6.py
+++ picow2/main6.py
@@ -21,12 +21,16 @@ from machine import Timer,ADC
 
 adc = machine.ADC(4)
 conversion_factor = 3.3 / (65535)
-def do_thing(t):
+def do_thing(t1):
     reading = adc.read_u16() * conversion_factor
     temperature = 27 - (reading - 0.706)/0.001721
     print()
     print(f"電壓:{reading}")
     print(f"溫度:{temperature}")
- 
 
-Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing )
+def do_thing1(t2):
+    print()
+    print("do_thing1")
+
+t1 = Timer(period=2000, mode=Timer.PERIODIC, callback=do_thing )
+t2 = Timer(period=500, mode=Timer.PERIODIC, callback=do_thing1 )
